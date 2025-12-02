dbb317e514a9662e7c90c15bf23b83a1489e18c9
ALBI ANIL <albi.p.anil2003@gmail.com>
2025-12-02 16:09:54 +0530

    chenges adeed

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -147,6 +147,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+   "https://fullstack-notes-app-j6iz.vercel.app/"
+]
+CSRF_TRUSTED_ORIGINS = [
+   "https://fullstack-notes-app-j6iz.vercel.app/"
+]
+
  
 
 
--- Backend/notes/models.py
+++ Backend/notes/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class NoteInfo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
